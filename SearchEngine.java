d3070a6c40644a088f5de43ff09cb85547074e5e
asdacdsfca <114449631+asdacdsfca@users.noreply.github.com>
2022-10-13 16:55:13 -0700

    revised

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,24 +2,46 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.*;
 
-class newHandler implements URLHandler {
-    ArrayList<String> stringSearch = new ArrayList<String>();
-    ArrayList<String> searchOutput = new ArrayList<String>();
-    if (url.getPath().contains("/add")) {
-        String[] parameters = url.getQuery().split("=");
-        if (parameters[0].equals("s")) {
-            stringSearch.add(paramters[1]);
-            return String.format("String has been addded", parameters[1], stringSearch);
-        }
-    }
-    if (url.getPath().contains("/search")) {
-        String[] parameters = url.getQuery().split("=");
-        if (paramters[0].equals("s")) {
-            for (i = 0, i< stringSearch.length, i++){
-                if stringSearch[i].contains(parameters[1]){
-                    searchOutput.add(parameters[1]);
-                    return String.format("Search Result:", parameters[1], searchOutput);
+class Search implements URLHandler {
+    ArrayList<String> result = new ArrayList<>();
+
+    public String handleRequest(URI url){
+        if (url.getPath().equals("/")) {
+            return result.toString();
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    result.add(parameters[1]);
+                    return result.toString();
                 }
             }
+            else if (url.getPath().contains("/search")) {
+                ArrayList<String> output = new ArrayList<>();
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    for (String i: result) {
+                        if (i.toUpperCase().contains(parameters[1].toUpperCase())) {
+                            output.add(i);
+                        }
+                    }
+                    return output.toString();
+                }
+            }
+            return "404 NOT Found!";
         }
     }
+}
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Search());
+    }
+}
